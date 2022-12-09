ef3d9a3b6c5b15eb9bcba1d1ff21a6054ed0d82b
Alejandro Villanueva <alejandro.villanueva@sysdig.com>
2022-12-09 21:53:28 +0100

    Day 4, second part.
    Signed-off-by: Alejandro Villanueva <alejandro.villanueva@sysdig.com>

--- 2022/04/day_04.adb
+++ 2022/04/day_04.adb
@@ -7,8 +7,9 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 procedure Day_04 is
 
     File                 : File_Type;
-    Line                 : Unbounded_String;
     F, S                 : Unbounded_String;
+    A, B, C, D           : Natural := 0;
+    Contained_Sections   : Natural := 0;
     Overlapping_Sections : Natural := 0;
 
     procedure Read_Section_Pair
@@ -21,24 +22,36 @@ procedure Day_04 is
         Second_Sec := To_Unbounded_String (Input (Last_Pos + 1 .. Input'Last));
     end Read_Section_Pair;
 
-    procedure Check_Containment (F, S : Unbounded_String) is
-        A, B, C, D, H1, H2 : Natural := 0;
-        Result             : Natural := 0;
+    procedure Extract_Ranges
+       (F, S : in Unbounded_String; A, B, C, D : out Natural)
+    is
+        H1, H2 : Natural := 0;
     begin
         Find_Token (F, To_Set ('-'), 1, Outside, H1, H2);
         A := Integer'Value (To_String (F) (H1 .. H2));
         Find_Token (F, To_Set ('-'), H2 + 1, Outside, H1, H2);
         B := Integer'Value (To_String (F) (H1 .. H2));
-
         Find_Token (S, To_Set ('-'), 1, Outside, H1, H2);
         C := Integer'Value (To_String (S) (H1 .. H2));
         Find_Token (S, To_Set ('-'), H2 + 1, Outside, H1, H2);
         D := Integer'Value (To_String (S) (H1 .. H2));
+    end Extract_Ranges;
 
+    procedure Check_Containment (F, S : Unbounded_String) is
+    begin
+        Extract_Ranges (F, S, A, B, C, D);
         if (A >= C and B <= D) or (C >= A and D <= B) then
-            Overlapping_Sections := Overlapping_Sections + 1;
+            Contained_Sections := Contained_Sections + 1;
         end if;
     end Check_Containment;
+
+    procedure Check_Overlapping (F, S : Unbounded_String) is
+    begin
+        Extract_Ranges (F, S, A, B, C, D);
+        if (A <= C and B >= C) or (A >= C and A <= D) then
+            Overlapping_Sections := Overlapping_Sections + 1;
+        end if;
+    end Check_Overlapping;
 
 begin
     Open (File, In_File, "input.txt");
@@ -46,7 +59,9 @@ begin
     while not End_Of_File (File) loop
         Read_Section_Pair (Get_Line (File), F, S);
         Check_Containment (F, S);
+        Check_Overlapping (F, S);
     end loop;
+    Put_Line ("Number of contained sections:  " & Contained_Sections'Image);
     Put_Line ("Number of overlapping sections:" & Overlapping_Sections'Image);
 
     Close (File);
